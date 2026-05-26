d3c2b4c023f317ae353c134e6d567865151eb225
zhou140550 <3244318424@qq.com>
2026-05-26 13:37:35 +0800

    Create [[path]].js

---
+++ functions/[[path]].js
@@ -0,0 +1,29 @@
+export async function onRequest(context) {
+  const request = context.request;
+  const url = new URL(request.url);
+  const target = 'https://open.feishu.cn' + url.pathname + url.search;
+
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  };
+
+  if (request.method === 'OPTIONS') {
+    return new Response(null, { headers: corsHeaders });
+  }
+
+  const response = await fetch(target, {
+    method: request.method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': request.headers.get('Authorization') || '',
+    },
+    body: ['GET', 'HEAD'].includes(request.method) ? null : request.body,
+  });
+
+  const data = await response.text();
+  return new Response(data, {
+    headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+  });
+}
