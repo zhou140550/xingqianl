03aca91e50c56d46b03da3a325e7c0e58cda18ae
zhou140550 <3244318424@qq.com>
2026-05-26 13:26:10 +0800

    Update and rename feishu.js to [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const request = context.request;
   const url = new URL(request.url);
-  const path = url.pathname.replace('/api/feishu', '');
+  const path = url.pathname.replace('/api', '');
   const target = 'https://open.feishu.cn' + path + url.search;
 
   const corsHeaders = {
